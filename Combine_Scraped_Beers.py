f4107152aab96d066afbe34679ca5f2f61014e1b
jblanche1325 <jblanche1325@gmail.com>
2020-11-21 17:10:44 -0500

    Made Combine_Scraped_Beers.py command line-friendly

--- Combine_Scraped_Beers.py
+++ Combine_Scraped_Beers.py
@@ -1,19 +1,18 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 import os
 import pandas as pd
 
-data_files = [f for f in os.listdir() if 'beer_table' in f]
+def combine_scraped_beers():
 
-for file in data_files:
-    beer_file = pd.read_csv(file, header=0)
-    big_beer_table.append(beer_file)
+    data_files = [f for f in os.listdir() if 'beer_table' in f]
+
+    for file in data_files:
+        beer_file = pd.read_csv(file, header=0)
+        big_beer_table.append(beer_file)
+
+    beer_df = pd.concat(big_beer_table, axis=0, ignore_index=True)
+
+    beer_df.to_csv('beer_df.csv', index=False)
     
-beer_df = pd.concat(big_beer_table, axis=0, ignore_index=True)
-
-beer_df.to_csv('beer_df.csv', index=False)
+if __name__ == '__main__':
+    combine_scraped_beers()
 
